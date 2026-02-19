9c5e80d5dd415c4b954d6c94f81d8b37484341b1
Joshua Wang <joshuawang9999@gmail.com>
2026-02-18 21:34:03 -0500

    finished first model using linear regression and added the corresponding data to data directory

--- src/models/LiverDisorderModel.py
+++ src/models/LiverDisorderModel.py
@@ -1,57 +1,55 @@
-#pip install ucimlrepo
-from ucimlrepo import fetch_ucirepo 
-
-import numpy as np
-
 import torch
 import torch.nn as nn 
-import torch.nn.functional as F 
 
 from torch.optim import Adam
-from torch.utils.data import TensorDataset, DataLoader
 
-import lightning as L
-  
-# fetch dataset 
-liver_disorders = fetch_ucirepo(id=60) 
-  
-# data (as pandas dataframes) 
-X = liver_disorders.data.features 
-y = liver_disorders.data.targets 
-
-# Convert to numpy
-X = X.to_numpy().astype(np.float32)
-y = y.to_numpy().astype(np.float32)
-
-# If y is shape (n,1) keep it that way
-if len(y.shape) == 1:
-    y = y.reshape(-1, 1)
-
-# Convert to torch tensors
-X_tensor = torch.tensor(X)
-y_tensor = torch.tensor(y)
-
-dataset = TensorDataset(X_tensor, y_tensor)
-loader = DataLoader(dataset, batch_size=32, shuffle=True)
-  
-# metadata 
-print(liver_disorders.metadata) 
-  
-# variable information 
-print(liver_disorders.variables) 
-
+#target is drinks: number of haf-pint equivalents of alchol per day
 class LiverDisorderModel(nn.Module):
     def __init__(self, input_size):
         super().__init__()
 
         self.model = nn.Sequential(
-            nn.Linear(input_size, 32),
+            nn.Linear(input_size, 64),
             nn.ReLU(),
-            nn.Linear(32, 16),
+            nn.Linear(64, 16),
             nn.ReLU(),
-            nn.Linear(16, 1),
-            nn.Sigmoid()  # Binary classification
+            nn.Linear(16, 1)
         )
+
+        self.optimizer = Adam(self.parameters(), lr=0.001)
+        self.criterion = nn.MSELoss()
 
     def forward(self, input):
         return self.model(input)
+    
+    def train_model(self, epochs, dataloader):
+        self.train()
+
+        for epoch in range(epochs):
+            total_loss = 0
+            for xb, yb in dataloader:
+                preds = self.forward(xb)
+                loss = self.criterion(preds, yb)
+
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+
+                total_loss += loss.item()
+            
+            avg_loss = total_loss / len(dataloader)
+            print(f"Epoch {epoch+1}, Train Loss: {avg_loss:.4f}")
+    
+    def evaluate(self, dataloader):
+        self.eval()
+        total_loss = 0
+
+        with torch.no_grad():
+            for xb, yb in dataloader:
+                preds = self.forward(xb)
+                loss = self.criterion(preds, yb)
+
+                total_loss += loss.item()
+        
+        avg_loss = total_loss / len(dataloader)
+        print(f"Loss: {avg_loss:.4f}")
